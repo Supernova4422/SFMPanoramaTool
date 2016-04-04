deeb12f65addf78eaaf3758a04d1c16388c83025
Fantasmos <andromedam51413>
2016-04-04 19:54:18 +1000

    General cleanups to the code

--- SFMPanoramaTool/SFMPanoramaTool/BatchRename.cs
+++ SFMPanoramaTool/SFMPanoramaTool/BatchRename.cs
@@ -15,41 +15,46 @@ namespace SFMPanoramaTool
             Console.WriteLine("Press enter to begin searching for the first file in the sequence");
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
-            if (openFileDialog1.ShowDialog()== DialogResult.OK)
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                
+
                 OpenFileDialog openFileDialog2 = new OpenFileDialog();
 
-                if (openFileDialog2.ShowDialog() == DialogResult.OK)
+                if (openFileDialog2.ShowDialog() == DialogResult.OK && (System.IO.Path.GetDirectoryName(openFileDialog2.FileName) == System.IO.Path.GetDirectoryName(openFileDialog1.FileName)))
                 {
                     string FirstFile = System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
                     string SecondFile = System.IO.Path.GetFileNameWithoutExtension(openFileDialog2.FileName);
                     string FilePath = System.IO.Path.GetDirectoryName(openFileDialog2.FileName);
+                    string extension = System.IO.Path.GetExtension(openFileDialog2.FileName);
 
                     for (int i = 0; i < FirstFile.Length; i++)
                     {
-                        if(!(FirstFile.Substring(i, 1).Equals(SecondFile.Substring(i, 1))))
+                        if (!(FirstFile.Substring(i, 1).Equals(SecondFile.Substring(i, 1))))
                         {
                             string FileName = SecondFile.Substring(0, i);
-                            
+
                             int increments = Int32.Parse(SecondFile.Substring(i));
 
-                            Console.WriteLine("The filename is {0} within the filepath {1} and the amount of files is: {2}. We also can say that {2} is a {3} digit number", FileName, FilePath, increments, Math.Floor(Math.Log10(increments) + 1));  
+                            Console.WriteLine("The filename is {0} within the filepath {1} and the amount of files is: {2}. We also can say that {2} is a {3} digit number", FileName, FilePath, increments, Math.Floor(Math.Log10(increments) + 1));
                             break;
                         }
-                        
+
                     };
-                    
-                    //  Console.WriteLine(openFileDialog2.FileName);
-                    //  Console.WriteLine(openFileDialog1.FileName);
+
                 }
-                
-                }
+                Console.WriteLine("Either you hit cancel or both Files selected are from different directories! Ensure they are in the same, then try again");
+
+            }
             else
             {
                 return;
             }
 
         }
+        public void RenameFiles()
+        {
+
+        }
     }
 }
+
