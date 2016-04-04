dd9619c4e0fac86618497eb607fb56846e62a3d4
fantasmos <benji.k@live.com.au>
2016-04-04 17:01:05 +1000

    Text Input Implementation
    Implemented console commands, PROPERLY

--- SFMPanoramaTool/SFMPanoramaTool/Program.cs
+++ SFMPanoramaTool/SFMPanoramaTool/Program.cs
@@ -10,6 +10,27 @@ namespace SFMPanoramaTool
     {
         static void Main(string[] args)
         {
+            const string CloseCommand = "close";
+            Console.WriteLine("Welcome to the SFM panorama toolkit alpha! There are likely to be some errors, so please be sure to report all of them.");
+            Console.WriteLine("To get started, type: /Help");
+            bool Exit = false;
+            do {
+                switch (Console.ReadLine().ToLowerInvariant()) //We'll return everything lower case, just to ensure case insensitvity
+                {
+                    case "/help":
+                        Console.WriteLine("{0} is able to close this window, that's pretty much it", CloseCommand);
+                        break;
+                    case CloseCommand:
+                        Exit = true;
+                        break;
+                }
+            }while(!Exit);
+        }
+        void GetFiles()
+        {
+            Console.WriteLine("Open the first file in the sequence please");
+
+            Console.WriteLine("Open the final file in the sequence please");
         }
     }
 }
