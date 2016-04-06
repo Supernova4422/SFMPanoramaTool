be3f039b171a3d7ad5c589797aed43f7dbc99538
Fantasmos <andromedam51413>
2016-04-06 20:26:06 +1000

    Improved Documentation and video writing establishing

---
+++ SFMPanoramaTool/VideoWriter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Accord;
+using Accord.Collections;
+namespace AForge.Video.FFMPEG
+{
+    public class VideoFileWriter : IDisposable
+    {
+        
+        static void VideoMaker()
+        {
+            VideoFileWriter writer = new VideoFileWriter();
+            
+        }
+    }
+}
