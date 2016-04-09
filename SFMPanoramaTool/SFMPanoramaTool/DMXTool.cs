cc99c44ff7734de316cece6453f73e2ce4446716
fantasmos <benji.k@live.com.au>
2016-04-09 14:08:42 +1000

    Injection
    Not going well trying to insert something with a unique GUID.

--- SFMPanoramaTool/SFMPanoramaTool/DMXTool.cs
+++ SFMPanoramaTool/SFMPanoramaTool/DMXTool.cs
@@ -14,7 +14,7 @@ namespace SFMPanoramaTool
     class DMXTool
     {
         
-        protected FileStream Binary_5_File = System.IO.File.OpenRead("D:/SFMPanorama/Datamodel.NET/Tests/Resources/sulfur_panotext.dmx");
+        protected FileStream Binary_5_File = System.IO.File.OpenRead("D:/sulfur_panotext.dmx");
 
         public void UpgradeDMX ()
         {
@@ -47,13 +47,81 @@ namespace SFMPanoramaTool
 
             SaveAndConvert(data, data.Encoding, data.EncodingVersion);
 
+            AddShots(data);
             //data.Root.Get<Element>("activeClip").Get<Element>("subClipTrackGroup").Get<ElementArray>("tracks")[0].Get<ElementArray>("children").Add(getclip);
             //  var data3 = data.Root.Get<Element>("shot1");
             return;
         }
         protected void SaveAndConvert(Datamodel.Datamodel dm, string encoding, int version)
         {
-            dm.Save("D:/SFMPanorama/Datamodel.NET/Tests/newpano.dmx", encoding, version);
+            dm.Save("D:/newpano.dmx", encoding, version);
         }
+        public DM AddShots (DM data)
+        {
+            DM Newdata = new DM();
+            //Set of cameras 
+            Quaternion[] Cameras =
+            {
+                Quaternion.CreateFromYawPitchRoll(0, 0, 0),
+                Quaternion.CreateFromYawPitchRoll(0, 0, 90),
+                Quaternion.CreateFromYawPitchRoll(0, 0, 180),
+                Quaternion.CreateFromYawPitchRoll(0, 0, -90),
+                Quaternion.CreateFromYawPitchRoll(0, 90, 0),
+                Quaternion.CreateFromYawPitchRoll(0, -90, 0)
+            };
+
+
+            int AmountOfShots = data.Root.Get<Element>("activeClip").Get<Element>("subClipTrackGroup").Get<ElementArray>("tracks")[0].Get<ElementArray>("children").Count;
+
+            //We loop 6 times, because we're making 6 angles
+            for (int i = 0; i < 6; i++)
+            {
+                int iterations = 0; //We do this to ensure that it doesn't infinitly loop by beginning processes on newly added shots
+                foreach (Element Shot in data.Root.Get<Element>("activeClip").Get<Element>("subClipTrackGroup").Get<ElementArray>("tracks")[0].Get<ElementArray>("children"))
+                {
+                    if (iterations == AmountOfShots)
+                    {
+                        break;
+                    }
+                    Shot.Get<Element>("camera").Get<Element>("transform").Remove("orientation"); //First we remove the current orientation
+                    Shot.Get<Element>("camera").Get<Element>("transform").Add("orientation", Cameras[i]); //Then we add the orientation for the increment we're at and store in the new camera
+
+                    var newshot = new Element(); //This generates both a new element AND a new ID
+
+
+                    data.Root.Get<Element>("activeClip").Get<Element>("subClipTrackGroup").Get<ElementArray>("tracks")[0].Get<ElementArray>("children").Add(newshot);
+                    int size = data.Root.Get<Element>("activeClip").Get<Element>("subClipTrackGroup").Get<ElementArray>("tracks")[0].Get<ElementArray>("children").
+                    
+                        
+                    //We do it like this to insert every value BUT the ID    
+                    foreach (KeyValuePair<string,object> Value in Shot)
+                    {
+                        if (Value.Key != "ID")
+                        {
+                            
+                            data.Root.Get<Element>("activeClip").Get<Element>("subClipTrackGroup").Get<ElementArray>("tracks")[0].Get<ElementArray>("children")[0] = Value.Value;
+                        }
+                    }
+                   
+
+                    
+
+
+                 //   Shot.ID = new System.Guid();
+                    data.Root.Get<Element>("activeClip").Get<Element>("subClipTrackGroup").Get<ElementArray>("tracks")[0].Get<ElementArray>("children").Add(Shot);
+                    data.Root.Get<Element>("activeClip").Get<Element>("subClipTrackGroup").Get<ElementArray>("tracks")[0].Get<ElementArray>("children").Add(newshot);
+
+                    Console.Write("Done1");
+                    iterations++;
+                }
+            }
+
+            //Console.WriteLine("break here");
+            return data;
+
+        }
+
+        
+
     }
 }
